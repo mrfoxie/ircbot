dec7e5f9055b3fe376aa067d7c06721fc5418736
mrfoxie <siddh.mistry396321@gmail.com>
2023-06-28 09:06:54 +0530

    basic command added

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ send_irc_message("NICK " + bot_name)
 send_irc_message("USER " + bot_name + " 0 * :" + bot_name)
 send_irc_message("JOIN " + channel)
 
+# Command handler functions
+def handle_greet(user):
+    response = f"Hello, {user}! How can I assist you?"
+    return response
+
+def handle_help():
+    response = "I'm here to help! Available commands: !greet, !help, !weather"
+    return response
+
 # Main loop to receive and process messages
 while True:
     data = irc.recv(2048).decode("utf-8")
@@ -28,10 +37,16 @@ while True:
         user = data.split('!', 1)[0][1:]
         message = ":".join(data.split(':')[2:])
         
-        # Process the message here and prepare a response
-        # You can add your own logic based on the received message
-        
-        response = "Hello, " + user + "! How are you?"
-        
-        # Send the response back to the channel
-        send_irc_message("PRIVMSG " + channel + " :" + response)
+        # Check if message is a command
+        if message.startswith("!"):
+            command = message.split()[0][1:].lower()
+            
+            # Process command
+            if command == "greet":
+                response = handle_greet(user)
+            elif command == "help":
+                response = handle_help()
+            # Add more command handlers here as needed
+            
+            # Send the response back to the channel
+            send_irc_message("PRIVMSG " + channel + " :" + response)
