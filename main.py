83e3dafb851ee3dbc8e4a3bbf1187a9805b4959c
Mr. Blogger <siddh.mistry396321@gmail.com>
2023-06-28 15:48:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def handle_clear(user, is_op):
         response = "You don't have permission to use this command."
     return response
 
-# Message filtering
+# Message filtering.
 prohibited_words = ["anal", "anus", "arse", "ass", "ballsack", "balls", "bastard", "bitch", "biatch", "bloody", "blowjob", "blow job", "bollock", "bollok", "boner", "boob", "bugger", "bum", "butt", "buttplug", "clitoris", "cock", "coon", "crap", "cunt", "damn", "dick", "dildo", "dyke", "fag", "feck", "fellate", "fellatio", "felching", "fuck", "f u c k", "fudgepacker", "fudge packer", "flange", "Goddamn", "God damn", "hell", "homo", "jerk", "jizz", "knobend", "knob end", "labia", "lmao", "lmfao", "muff", "nigger", "nigga", "omg", "penis", "piss", "poop", "prick", "pube", "pussy", "queer", "scrotum", "sex", "shit", "s hit", "sh1t", "slut", "smegma", "spunk", "tit", "tosser", "turd", "twat", "vagina", "wank", "whore", "wtf"]
 prohibited_pattern = re.compile(r"\b(" + "|".join(prohibited_words) + r")\b", re.IGNORECASE)
 
